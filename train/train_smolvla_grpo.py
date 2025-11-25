7c69d5912dcb57a9d3e08db1a849a3829c2628d3
TomoCoder123 <thomasxdeng@gmail.com>
2025-11-25 02:35:21 -0800

    Refactored third party stuff and redid dependencies due to conflicts. First pass at setting up LIBERO simulation env

--- train/train_smolvla_grpo.py
+++ train/train_smolvla_grpo.py
@@ -1,18 +1,55 @@
-
 import torch
-import torch.nn as nn
-import json
-from lerobot.policies.pretrained import PreTrainedPolicy
+from torch import nn
 from lerobot.policies.smolvla.modeling_smolvla import SmolVLAPolicy
+from LIBERO.libero.libero import benchmark
+import os
+from LIBERO.libero.libero.envs import OffScreenRenderEnv
+
+# from libero README getting started
+def make_libero_env():
+    benchmark_dict = benchmark.get_benchmark_dict()
+    task_suite_name = "libero_10" # can also choose libero_spatial, libero_object, etc.
+    task_suite = benchmark_dict[task_suite_name]()
+
+    # retrieve a specific task
+    task_id = 0
+    task = task_suite.get_task(task_id)
+    task_description = task.language
+    task_bddl_file = os.path.join(benchmark.get_libero_path("bddl_files"), task.problem_folder, task.bddl_file)
+    print(f"[info] retrieving task {task_id} from suite {task_suite_name}, the " + \
+        f"language instruction is {task_description}, and the bddl file is {task_bddl_file}")
+
+    # step over the environment
+    env_args = {
+        "bddl_file_name": task_bddl_file,
+        "camera_heights": 128,
+        "camera_widths": 128
+    }
+    env = OffScreenRenderEnv(**env_args)
+    env.seed(0)
+    env.reset()
+    init_states = task_suite.get_task_init_states(task_id) # for benchmarking purpose, we fix the a set of initial states
+    init_state_id = 0
+    env.set_init_state(init_states[init_state_id])
+    return env
+
+#    dummy_action = [0.] * 7
+#    for step in range(10):
+#        obs, reward, done, info = env.step(dummy_action)
+#        q = env.sim.data.qvel.copy()
+#        print(q)
+#    env.close()
 
 def main():
-    policy = SmolVLAPolicy.from_pretrained(
-        "HuggingFaceVLA/smolvla_libero")
-    
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    policy.to(device)
-
-    policy.train()
+    # DEBUG
+    policy = nn.Linear(768, 1)
+#    policy = SmolVLAPolicy.from_pretrained(
+#        "HuggingFaceVLA/smolvla_libero")
+#    
+#    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#    policy.to(device)
+#
+#    policy.train()
 
     optimizer = torch.optim.AdamW(
         policy.parameters(),
@@ -20,9 +57,7 @@ def main():
         betas=(0.9, 0.95)
     )
     
-    
-    env = make_libero_env("libero_spatial")
-
+    env = make_libero_env()
 
 if __name__ == "__main__":
     main()
