568c47ecb31d56f6e8e6390a77d395e69e8bd1c5
TomoCoder123 <thomasxdeng@gmail.com>
2025-12-05 02:26:13 -0800

    un made package false

---
+++ experiments/mech_interp_experiments.py
@@ -0,0 +1,70 @@
+# run_experiment.py
+
+import torch
+from model.smolvla_policy import SmolVLALiberoPolicy
+from env.env import make_libero_env, snapshot_obs, get_agentview_frame, get_wrist_frame
+import imageio
+import os
+FPS = 60
+
+TASK_SUITE_NAME = "libero_spatial" 
+STEPS = 280 #lerobot libero default for object, 520 is default for long
+def main():
+    print("CUDA_VISIBLE_DEVICES:", os.environ.get("CUDA_VISIBLE_DEVICES"))
+    print("torch.cuda.device_count():", torch.cuda.device_count())
+    assert torch.cuda.device_count() == 1, "More than one CUDA device available which will lead to runtime device mismatches"
+
+    # Load SmolVLA policy
+    policy = SmolVLALiberoPolicy(
+        "HuggingFaceVLA/smolvla_libero", device="cuda"
+    )
+
+    # If you want to train it (e.g., GRPO)
+    optimizer = torch.optim.AdamW(
+        policy.parameters(),
+        lr=1e-5,
+        betas=(0.9, 0.95)
+    )
+
+    env, language = make_libero_env(TASK_SUITE_NAME, task_id=1)
+    print(f"Task description: {language}")
+    obs = env.reset()
+    snapshot_obs(obs, "before.png")
+    
+    agentview_path = "agentview.mp4"
+    wristcam_path = "wristcam.mp4"
+
+    agent_writer = imageio.get_writer(agentview_path, fps=FPS)
+    wrist_writer = imageio.get_writer(wristcam_path, fps=FPS)
+
+    print(f"Recording agentview -> {agentview_path}")
+    print(f"Recording wristcam -> {wristcam_path}")
+    
+    # Write first frames
+    agent_writer.append_data(get_agentview_frame(obs))
+    wrist_writer.append_data(get_wrist_frame(obs))
+    
+    # simple rollout. For GRPO we can rollout with no_grad but will need grads when recomputing new model log densities for chosen actions
+    # print out first and last end-effector postitions
+    for step in range(STEPS):
+        if step == 1:
+            print (obs["robot0_eef_pos"])
+
+        with torch.no_grad():
+            action = policy.get_action(obs, language)
+            action = action.cpu().clone().detach().tolist()[0]
+        obs, reward, done, info = env.step(action)
+        
+        print(f"Step {step} | Reward: {reward:.3f}")
+            
+        agent_writer.append_data(get_agentview_frame(obs))
+        wrist_writer.append_data(get_wrist_frame(obs))
+        if done:
+            print (obs["robot0_eef_pos"])
+            break
+
+    snapshot_obs(obs, "after.png")
+    env.close()
+
+if __name__ == "__main__":
+    main()
